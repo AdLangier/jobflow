651bda45bce3b8f992cdfdf6f2d6510bf3aa0804
Adrian Langier <langier.adrian@gmail.com>
2020-09-08 13:03:32 +0200

    added example website

--- src/main/java/com/example/jobflow/model/Website.java
+++ src/main/java/com/example/jobflow/model/Website.java
@@ -24,4 +24,8 @@ public class Website {
 
     private String url;
 
+    public Website(String name, String url) {
+        this.name = name;
+        this.url = url;
+    }
 }
--- src/main/java/com/example/jobflow/repository/WebsiteRepository.java
+++ src/main/java/com/example/jobflow/repository/WebsiteRepository.java
@@ -1,7 +1,16 @@
 package com.example.jobflow.repository;
 
 import com.example.jobflow.model.Website;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface WebsiteRepository extends CrudRepository<Website,Integer> {
+
+
+    @Query(value = "SELECT * FROM website WHERE name=?1 LIMIT 1",nativeQuery = true)
+    Optional<Website> findWebsite(String name);
+
+
 }
