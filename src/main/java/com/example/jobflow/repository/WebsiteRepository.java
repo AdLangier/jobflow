3e936c57e2e1e583472643a31b02fa692bffcac4
Adrian Langier <langier.adrian@gmail.com>
2020-09-08 11:56:04 +0200

    added database structure

---
+++ src/main/java/com/example/jobflow/repository/WebsiteRepository.java
@@ -0,0 +1,7 @@
+package com.example.jobflow.repository;
+
+import com.example.jobflow.model.Website;
+import org.springframework.data.repository.CrudRepository;
+
+public interface WebsiteRepository extends CrudRepository<Website,Integer> {
+}
